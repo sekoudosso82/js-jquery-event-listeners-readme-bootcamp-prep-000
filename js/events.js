3f48f45ccf78bf16cdd258f6d2214f0f9af8314a
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 18:15:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,51 @@
-//define functions here
+
+
+function getIt()
+{
+  $("p").on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt()
+{
+  $("img").on("load", function(){
+    $("img").addClass("tasty")
+  })
+}
+
+
+function pressIt()
+{
+  $("input").on("keydown", function(key) {
+  if ( key.which == 71 ) {
+    alert('you entered G');
+    return;
+  }});
+}
+
+function submitIt()
+{
+  $("form").on("submit", function() {
+  
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
 
 $(document).ready(function(){
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
-// call functions here
+$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 
 });
